4539476c6d5ed37f4e4daa343b33d03dfa1c2d67
Stephen carter <stephen.carter@mail.mcgill.ca>
2015-12-05 14:15:29 -0500

    receive works in basic test

--- src/main.c
+++ src/main.c
@@ -4,17 +4,55 @@
 
 #define osObjectsPublic                     // define objects in main module
 
-#include "all_wireless_tests.h"
+#include "osObjects.h" 
+
+#include "stm32f4xx.h"                  // Device header
+#include "stm32f4xx_conf.h"
+#include "stm32f429i_discovery.h"
+#include <stdio.h>
+#include <string.h>
+#include "cc2500.h"
 
 
+const uint8_t PARTNUM = 128, VERSION = 3;
 
-static void delay(__IO uint32_t nCount)
-{
-  __IO uint32_t index = 0; 
-  for(index = 100000*nCount; index != 0; index--)
-  {
-  }
+uint8_t recv_buffer[20];
+uint8_t num;
+int TEST_VALUE = 0;
+void EXTI4_IRQHandler() {
+	int i;
+	if (EXTI_GetITStatus(CC2500_SPI_INT1_EXTI_LINE) != RESET){
+		CC2500_Read_SR(&num, 0x3B);
+		CC2500_Read_RX(recv_buffer, num);
+		for(i = 0; i < num; i++){
+			printf("%d, ", recv_buffer[i]);
+		}
+		printf("\n");
+		EXTI_ClearITPendingBit(CC2500_SPI_INT1_EXTI_LINE);
+	}
 }
+
+
+void test_control_read(void) {
+	uint8_t x;
+	CC2500_Read_SR(&x, CC2500_PARTNAME);
+	printf("%d\t", x);
+	CC2500_Read_SR(&x, CC2500_VERSION);
+	printf("%d\n", x);
+}
+
+
+void test_wireless(void){
+
+	uint8_t res2[3];
+	
+	CC2500_Read(res2, CC2500_FREQ_REG, 3);
+	printf("result = %x, %x, %x\n", res2[0], res2[1], res2[2]);
+
+	CC2500_INT_INIT();
+	for (int i=0; i<168000000/6; i++);
+	while(1);
+}	
 
 void transmit_data(void const *argument){
 		test_control_read();
