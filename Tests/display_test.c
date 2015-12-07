7d19569af1b08654227364d48e77291cade085ad
Jit <kanetkarster@gmail.com>
2015-12-07 12:50:25 -0500

    add project files

--- Tests/display_test.c
+++ Tests/display_test.c
@@ -3,8 +3,8 @@
 
 
 void display_test(){
-	uint16_t buffer[4] = {0, 6, 1, 1};
-	for (int i = 0; i < 4; i=i+2){
+	uint16_t buffer[8] = {0, 7, 7, 3, 6, 1, 5, 2};
+	for (int i = 0; i < 8; i=i+2){
 		draw_update(buffer[i], buffer[i+1]);
 	}
 }
